f1ad9b7f4bea59d96bab402c737e3df398bae4a1
AleG <aleguizzo@icloud.com>
2021-12-23 17:03:18 +0100

    commit prima versione alpha

--- contatoreios/ViewController.swift
+++ contatoreios/ViewController.swift
@@ -13,7 +13,32 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    @IBOutlet weak var stepperOne: UIStepper!
+    @IBOutlet weak var one: UILabel!
+    @IBOutlet weak var stepperTwo: UIStepper!
+    @IBOutlet weak var two: UILabel!
+    @IBAction func stepperOneChanged(_ sender: Any) {
+        updateScores()
+    }
+    
+    @IBAction func stepperTwoChanged(_ sender: Any) {
+        updateScores()
+    }
+    func updateScores() {
+        one.text = String(Int(stepperOne.value))
+        two.text = String(Int(stepperTwo.value))
 
-
+    }
+    @IBAction func reset(_ sender: Any) {
+        stepperOne.value = 0
+        stepperTwo.value = 0
+        updateScores()
+    }
+    @IBAction func info(_ sender: Any) {
+        let alert = UIAlertController(title: "Contatore", message: "version 1.0alpha\n©2021 AleGSoftware\n\nAlessandro Guizzetti", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
+        NSLog("The \"OK\" alert occured.")
+        }))
+        self.present(alert, animated: true, completion: nil)
+    }
 }
-
