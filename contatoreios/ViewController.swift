5a684894b0a54954e67a4d6a1aa6db996d33e304
AleG <aleguizzo@icloud.com>
2021-12-24 07:38:18 +0100

    miglioramenti per beta 1

--- contatoreios/ViewController.swift
+++ contatoreios/ViewController.swift
@@ -12,11 +12,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateScores()
+        updateMaxScore(to: 5.0)
     }
     @IBOutlet weak var stepperOne: UIStepper!
     @IBOutlet weak var one: UILabel!
     @IBOutlet weak var stepperTwo: UIStepper!
     @IBOutlet weak var two: UILabel!
+    @IBOutlet weak var stepperMax: UIStepper!
+    @IBOutlet weak var max: UILabel!
     @IBAction func stepperOneChanged(_ sender: Any) {
         updateScores()
     }
@@ -24,10 +28,25 @@ class ViewController: UIViewController {
     @IBAction func stepperTwoChanged(_ sender: Any) {
         updateScores()
     }
+    @IBAction func stepperMaxChanged(_ sender: Any) {
+        updateMaxScore(to: stepperMax.value)
+    }
     func updateScores() {
         one.text = String(Int(stepperOne.value))
         two.text = String(Int(stepperTwo.value))
+        isThereAWinner()
 
+    }
+    func isThereAWinner(){
+        if stepperOne.value == stepperMax.value{
+            andTheWinnerIs(player: 1)
+            one.text = "⭕️"
+            two.text = "❌"
+        } else if stepperTwo.value == stepperMax.value{
+            andTheWinnerIs(player: 2)
+            two.text = "⭕️"
+            one.text = "❌"
+        }
     }
     @IBAction func reset(_ sender: Any) {
         stepperOne.value = 0
@@ -35,7 +54,19 @@ class ViewController: UIViewController {
         updateScores()
     }
     @IBAction func info(_ sender: Any) {
-        let alert = UIAlertController(title: "Contatore", message: "version 1.0alpha\n©2021 AleGSoftware\n\nAlessandro Guizzetti", preferredStyle: .alert)
+        let alert = UIAlertController(title: "Contatore", message: "versione 1.0 Beta 1\n © 2021 AleGSoftware\n\nAlessandro Guizzetti", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
+        NSLog("The \"OK\" alert occured.")
+        }))
+        self.present(alert, animated: true, completion: nil)
+    }
+    func updateMaxScore(to: Double){
+        max.text = String(Int(to))
+        stepperOne.maximumValue = to
+        stepperTwo.maximumValue = to
+    }
+    func andTheWinnerIs(player: Int){
+        let alert = UIAlertController(title: "Vincitore", message: "Il vincitore è il giocatore " + String(player), preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
         NSLog("The \"OK\" alert occured.")
         }))
